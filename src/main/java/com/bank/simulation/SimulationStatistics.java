4721477e97de66fc4ea52f0ea9d27f975876d917
Mohey Karam <moheykaramdev@gmail.com>
2025-11-14 18:05:35 +0200

    Added ability to merge SimulationStatistics

--- src/main/java/com/bank/simulation/SimulationStatistics.java
+++ src/main/java/com/bank/simulation/SimulationStatistics.java
@@ -1,6 +1,10 @@
 package com.bank.simulation;
 
+import java.util.ArrayList;
+
 public class SimulationStatistics {
+    public record Statistic(String label, Number value) {}
+
     private int totalCashServiceTime = 0;
     private int totalServiceServiceTime = 0;
 
@@ -24,6 +28,8 @@ public class SimulationStatistics {
     private int totalIndoorTellerIdleTime = 0;
     private int totalOutdoorTellerIdleTime = 0;
     private int totalServiceEmployeeIdleTime = 0;
+
+    private ArrayList<Statistic> statistics;
 
     public void addTotalCashServiceTime(int time) {
         totalCashServiceTime += time;
@@ -97,7 +103,91 @@ public class SimulationStatistics {
         this.totalTime = totalTime;
     }
 
-    public void addSimulationStatistics(SimulationStatistics stats) {
+    public void calculateStatistics() {
+        statistics = new ArrayList<>();
+
+        // Stat 1
+        int totalCashCustomers = totalIndoorTellerCustomers + totalOutdoorTellerCustomers;
+        double avgCashCustomerServiceTime = totalCashCustomers == 0 ? 0 : totalCashServiceTime / (double) totalCashCustomers;
+        double avgServiceCustomerServiceTime = totalServiceEmployeeCustomers == 0 ? 0 : totalServiceServiceTime / (double) totalServiceEmployeeCustomers;
+
+        statistics.add(new Statistic("Average Cash Customer Service Time", avgCashCustomerServiceTime));
+        statistics.add(new Statistic("Average Service Customer Service Time", avgServiceCustomerServiceTime));
+
+        // Stat 2
+        double avgIndoorTellerWait = totalIndoorTellerWaitingCustomers == 0 ? 0 :
+                totalIndoorTellerWaitTime / (double) totalIndoorTellerWaitingCustomers;
+        double avgOutdoorTellerWait = totalOutdoorTellerWaitingCustomers == 0 ? 0 :
+                totalOutdoorTellerWaitTime / (double) totalOutdoorTellerWaitingCustomers;
+        double avgServiceEmployeeWait = totalServiceEmployeeWaitingCustomers == 0 ? 0 :
+                totalServiceEmployeeWaitTime / (double) totalServiceEmployeeWaitingCustomers;
+
+        double totalWaitingCustomers = totalIndoorTellerWaitingCustomers + totalOutdoorTellerWaitingCustomers + totalServiceEmployeeWaitingCustomers;
+        double totalWaitTime = totalIndoorTellerWaitTime + totalOutdoorTellerWaitTime + totalServiceEmployeeWaitTime;
+        double avgTotalWaitTime = totalWaitingCustomers == 0 ? 0 : totalWaitTime / totalWaitingCustomers;
+
+        statistics.add(new Statistic("Average Indoor Teller Wait Time", avgIndoorTellerWait));
+        statistics.add(new Statistic("Average Outdoor Teller Wait Time", avgOutdoorTellerWait));
+        statistics.add(new Statistic("Average Service Employee Wait Time", avgServiceEmployeeWait));
+        statistics.add(new Statistic("Average Total Wait Time", avgTotalWaitTime));
+
+        // Stat 3
+        statistics.add(new Statistic("Max Indoor Teller Queue Size", maxIndoorTellerQueueSize));
+        statistics.add(new Statistic("Max Outdoor Teller Queue Size", maxOutdoorTellerQueueSize));
+        statistics.add(new Statistic("Max Service Employee Queue Size", maxServiceEmployeeQueueSize));
+
+        // Stat 4
+        double probIndoorWait = totalIndoorTellerCustomers == 0 ? 0 :
+                totalIndoorTellerWaitingCustomers / (double) totalIndoorTellerCustomers;
+
+        double probOutdoorWait = totalOutdoorTellerCustomers == 0 ? 0 :
+                totalOutdoorTellerWaitingCustomers / (double) totalOutdoorTellerCustomers;
+
+        double probServiceWait = totalServiceEmployeeCustomers == 0 ? 0 :
+                totalServiceEmployeeWaitingCustomers / (double) totalServiceEmployeeCustomers;
+
+        statistics.add(new Statistic("Indoor Teller Wait Probability", probIndoorWait));
+        statistics.add(new Statistic("Outdoor Teller Wait Probability", probOutdoorWait));
+        statistics.add(new Statistic("Service Employee Wait Probability", probServiceWait));
+
+        // State 5
+        double indoorIdlePortion = totalTime == 0 ? 0 : totalIndoorTellerIdleTime / (double) totalTime;
+        double outdoorIdlePortion = totalTime == 0 ? 0 : totalOutdoorTellerIdleTime / (double) totalTime;
+        double serviceIdlePortion = totalTime == 0 ? 0 : totalServiceEmployeeIdleTime / (double) totalTime;
+
+        statistics.add(new Statistic("Indoor Tellers Idle Portion", indoorIdlePortion));
+        statistics.add(new Statistic("Outdoor Tellers Idle Portion", outdoorIdlePortion));
+        statistics.add(new Statistic("Service Employees Idle Portion", serviceIdlePortion));
+    }
+
+    public void merge(SimulationStatistics stats) {
+        this.totalCashServiceTime += stats.totalCashServiceTime;
+        this.totalServiceServiceTime += stats.totalServiceServiceTime;
+
+        this.totalIndoorTellerWaitTime += stats.totalIndoorTellerWaitTime;
+        this.totalOutdoorTellerWaitTime += stats.totalOutdoorTellerWaitTime;
+        this.totalServiceEmployeeWaitTime += stats.totalServiceEmployeeWaitTime;
+
+        this.maxIndoorTellerQueueSize = Math.max(this.maxIndoorTellerQueueSize, stats.maxIndoorTellerQueueSize);
+        this.maxOutdoorTellerQueueSize = Math.max(this.maxOutdoorTellerQueueSize, stats.maxOutdoorTellerQueueSize);
+        this.maxServiceEmployeeQueueSize = Math.max(this.maxServiceEmployeeQueueSize, stats.maxServiceEmployeeQueueSize);
+
+        this.totalIndoorTellerWaitingCustomers += stats.totalIndoorTellerWaitingCustomers;
+        this.totalOutdoorTellerWaitingCustomers += stats.totalOutdoorTellerWaitingCustomers;
+        this.totalServiceEmployeeWaitingCustomers += stats.totalServiceEmployeeWaitingCustomers;
+
+        this.totalIndoorTellerCustomers += stats.totalIndoorTellerCustomers;
+        this.totalOutdoorTellerCustomers += stats.totalOutdoorTellerCustomers;
+        this.totalServiceEmployeeCustomers += stats.totalServiceEmployeeCustomers;
+
+        this.totalTime += stats.totalTime;
+        this.totalIndoorTellerIdleTime += stats.totalIndoorTellerIdleTime;
+        this.totalOutdoorTellerIdleTime += stats.totalOutdoorTellerIdleTime;
+        this.totalServiceEmployeeIdleTime += stats.totalServiceEmployeeIdleTime;
+    }
+
+    public ArrayList<Statistic> getStatistics() {
+        return statistics;
     }
 
     @Override
